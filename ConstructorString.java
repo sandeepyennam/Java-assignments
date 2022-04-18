14a7f3193ae80dbba6a71aa2a23b13895c4c7f1d
sandeepyennam <102643401+sandeepyennam@users.noreply.github.com>
2022-04-18 21:22:28 +0530

    Delete ConstructorString.java

--- ConstructorString.java
+++
@@ -1,6 +0,0 @@
-public class ConstructorString {
-    public ConstructorString(String a)
-    {
-        System.out.println("this is the constructor of the class ConstructorString,the argument passed is"+a);
-    }
-}
