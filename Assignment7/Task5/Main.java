b930975981f95207ce03ece48369b47d5a9df9ac
sandeepyennam <102643401+sandeepyennam@users.noreply.github.com>
2022-04-19 10:08:42 +0530

    Add files via upload

---
+++ Assignment7/Task5/Main.java
@@ -0,0 +1,13 @@
+package Assignment7.Task5;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Shapes2.Rectangle1 rectangle=new Shapes2.Rectangle1();
+           rectangle.area(15);
+
+    }
+
+}
